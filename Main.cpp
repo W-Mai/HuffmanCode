2f22e543f44c22629ea11f369f4c6f43be3316e8
W-Mai <1341398182@qq.com>
2022-11-24 19:35:46 +0800

    🆕(Main): update usage
    Signed-off-by: W-Mai <1341398182@qq.com>

--- Main.cpp
+++ Main.cpp
@@ -3,44 +3,37 @@
 
 HuffmanTree* huffmanTree;
 
+typedef std::map<uint8_t, ElemType> WordFreq;
+
+WordFreq wordFreq(std::string str);
+
 int main() {
     huffmanTree = new HuffmanTree;
 
-    ElemType freq_table[] = {
-        {'H',  1     },
-        { 'a', 9     },
-        { 'p', 99    },
-        { 'P', 11    },
-        { 'y', 2     },
-        { ' ', 3     },
-        { 'B', 19    },
-        { 'i', 91    },
-        { 'r', 12    },
-        { 't', 31    },
-        { 'h', 999   },
-        { 'd', 112   },
-        { 'a', 319   },
-        { 'Y', 999112},
-        { ' ', 3     },
-        { 'T', 199   },
-        { 'o', 9112  },
-        { ' ', 3     },
-        { 'M', 1999  },
-        { 'e', 1123  },
-    };
+    auto str        = std::string("This is a HuffmanCode example");
+    auto freq_table = wordFreq(str);
 
     for (auto& freq : freq_table) {
-        huffmanTree->push(freq);
+        huffmanTree->push(freq.second);
     }
 
     huffmanTree->build();
 
     for (auto& freq : freq_table) {
-        printf("Raw: %c\tPrefixCode: %s\n", freq.ch, huffmanTree->preCode(freq.ch).c_str());
+        printf("Raw: %c\tPrefixCode: %s\n", freq.second.ch, huffmanTree->preCode(freq.second.ch).c_str());
     }
 
     printf("WPL: %f", huffmanTree->wpl());
 
     delete huffmanTree;
     return 0;
+}
+
+WordFreq wordFreq(std::string str) {
+    WordFreq freq;
+    for (auto& ch : str) {
+        freq[ch].ch = ch;
+        freq[ch].weight++;
+    }
+    return freq;
 }
